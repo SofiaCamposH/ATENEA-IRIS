58aad55d59b212912fa2fb866c07caf4defa8e7f
Carlos Galindo <utm22030587@utma.edu.mx>
2024-10-20 21:39:16 -0600

    feat: add qrcode screen

---
+++ src/react-native-app/src/screens/qrcode.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function QrCodeScreen() {
+    return (
+        <View>
+            <Text>QrCodeScreen</Text>
+        </View>
+    );
+}
